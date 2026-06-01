484ef048ac41dbf39a14c3c08a19e3c9b374d3e0
Martijn Jacobs <martijn@mj026.nl>
2026-06-01 11:10:19 +0200

    Fix nasty printing hassle

--- templates/revealjs-presentation/src/main.ts
+++ templates/revealjs-presentation/src/main.ts
@@ -5,6 +5,11 @@ import Highlight from 'reveal.js/plugin/highlight';
 const deck = new Reveal({
   plugins: [Markdown, Highlight],
 });
+
+deck.on('pdf-ready', function() {
+  const tag = document.createElement('style');
+  tag.appendChild(document.createTextNode("@page {size: a4 landscape;}"));
+  document.head.appendChild(tag);
+});
+
 deck.initialize();
-
-
