a6b4c2a3005c48f6032c67616d7ceb1d7f5e9e34
Martijn Jacobs <martijn@mj026.nl>
2026-06-15 18:56:15 +0200

    Update revealjs template

--- templates/revealjs-presentation/src/main.ts
+++ templates/revealjs-presentation/src/main.ts
@@ -1,13 +1,13 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown';
-import Highlight from 'reveal.js/plugin/highlight';
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown";
+import Highlight from "reveal.js/plugin/highlight";
 
 const deck = new Reveal({
   plugins: [Markdown, Highlight],
 });
 
-deck.on('pdf-ready', function() {
-  const tag = document.createElement('style');
+deck.on("pdf-ready", function () {
+  const tag = document.createElement("style");
   tag.appendChild(document.createTextNode("@page {size: a4 landscape;}"));
   document.head.appendChild(tag);
 });
